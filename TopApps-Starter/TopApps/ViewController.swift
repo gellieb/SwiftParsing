c4d80d75edbbd56509c655bf131cc243aff8bb15
Angel Baek <angelbaek1@gmail.com>
2015-05-26 17:40:29 -0400

    parse JSON the native swift way

--- TopApps-Starter/TopApps/ViewController.swift
+++ TopApps-Starter/TopApps/ViewController.swift
@@ -12,7 +12,34 @@ import UIKit
 class ViewController: UIViewController {
   
   override func viewDidLoad() {
-    super.viewDidLoad()    
-  }
+    super.viewDidLoad()
+    
+    DataManager.getTopAppsDataFromFileWithSuccess { (data) -> Void in
+        // Get the number 1 app using optional binding and NSJSONSerialization
+        //1
+        var parseError: NSError?
+        let parsedObject: AnyObject? = NSJSONSerialization.JSONObjectWithData(data,
+            options: NSJSONReadingOptions.AllowFragments,
+            error:&parseError)
+        
+        //2
+        if let topApps = parsedObject as? NSDictionary {
+            if let feed = topApps["feed"] as? NSDictionary {
+                if let apps = feed["entry"] as? NSArray {
+                    if let firstApp = apps[0] as? NSDictionary {
+                        if let imname = firstApp["im:name"] as? NSDictionary {
+                            if let appName = imname["label"] as? NSString {
+                                //3
+                                println("Optional Binding: \(appName)")
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+    
+    
+    
+    }
 }
-
